4edeb2d501bbca4179f26dc91a26cf8151d68f65
Mahfujul Huq <133667226+mahfuz0001@users.noreply.github.com>
2023-11-09 14:01:49 +0000

    sodfhj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,6 @@ import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
-
 export default function HomePage() {
   return (
     <main>
@@ -25,7 +16,6 @@ export default function HomePage() {
       </Head>
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
           <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
           <p className='mt-2 text-sm text-gray-800'>
             A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
